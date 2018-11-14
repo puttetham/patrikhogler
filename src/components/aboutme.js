4cfdddf2cafcc33eec45262cb9f7efb0903762b6
puttetham <patrikhogler@hotmail.com>
2018-11-14 19:09:14 +0100

    playing

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './aboutme.css';
 
 class About extends Component {
     render(){
         return(
             <div>
-                <h1>About Page</h1>
+                <h1 className="aboutHeading">About Page</h1>
             </div>
         )
     }
